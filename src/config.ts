c16180e63bd536ed6c11ad99defa4824a40f351d
goldencloudinnovations <263064006+goldencloudinnovations@users.noreply.github.com>
2026-09-15 12:37:28 -0700

    feat: add portal runtime config

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://ugbindlzyqaktejbxalk.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_NoOtqlUoD09Ikhmsmd3ShA_xI9IYmlJ';
+export const PORTAL_ORIGIN = window.location.origin;
